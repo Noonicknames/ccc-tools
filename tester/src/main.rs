bf4b77c83cf45d7ef07cbbe588a5458f46689c19
Noon <noonicknames@outlook.com>
2025-11-27 23:28:35 +0800

    tester

---
+++ tester/src/main.rs
@@ -0,0 +1,9 @@
+use nalgebra::DVector;
+
+fn main() {
+    let x_vec = DVector::from_vec(vec![1.0, 2.0, 4.0]);
+    let y_vec = DVector::from_vec(vec![1.0, 2.0, 1.0]);
+
+
+    println!("Hello, world!");
+}
